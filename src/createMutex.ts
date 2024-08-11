3f379b3c247a26064b91dbc9647af37da3b93a99
117 <16513382+117@users.noreply.github.com>
2024-08-11 01:20:38 -0400

    add jsdoc

--- src/createMutex.ts
+++ src/createMutex.ts
@@ -1,8 +1,21 @@
+/** @module createMutex */
+
+/**
+ * Represents a mutex object with methods to acquire and release the lock.
+ * @property {() => Promise<void>} acquire - Acquires the mutex.
+ * @property {() => void} release - Releases the mutex.
+ */
 type Mutex = {
+    /** acquires the mutex */
     acquire: () => Promise<void>;
+    /** releases the mutex */
     release: () => void;
 };
 
+/**
+ * Creates a mutex object with acquire and release methods.
+ * @returns {Mutex} A mutex object with acquire and release methods.
+ */
 const createMutex = (): Mutex => {
     let locked = false;
     const waiting: (() => void)[] = [];
